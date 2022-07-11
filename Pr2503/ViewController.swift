75e75d76db30e1549404155fcdf72de062f7c59f
Andrik <masmay.pl@gmail.com>
2022-07-11 23:15:50 +0200

    add buttons, text field and label

--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -1,44 +1,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
     
-    var isBlack: Bool = false {
-        didSet {
-            if isBlack {
-                self.view.backgroundColor = .black
-            } else {
-                self.view.backgroundColor = .white
-            }
-        }
+    let lenPassword = 3
+    
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var activiti: UIActivityIndicatorView!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    @IBAction func cancelBut(_ sender: Any) {
+        label.text = ""
+        textField.text = ""
+    }
+    
+    @IBAction func searchBut(_ sender: Any) {
+        self.bruteForce(passwordToUnlock: textField.text ?? "")
+        label.text = textField.text
+        activiti.hidesWhenStopped = true
+        activiti.stopAnimating()
+        textField.isSecureTextEntry = false
     }
     
     @IBAction func onBut(_ sender: Any) {
-        isBlack.toggle()
+        let passwordCharacters = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+        let randomPassword = String((0..<lenPassword).map{ _ in passwordCharacters[Int(arc4random_uniform(UInt32(passwordCharacters.count)))]})
+        textField.text = randomPassword
+        textField.isSecureTextEntry = true
+//        activiti.startAnimating()
+        label.text = "Password generated"
+        print(randomPassword)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activiti.hidesWhenStopped = true
         
-        
-        self.bruteForce(passwordToUnlock: "1!gr")
-        
-        // Do any additional setup after loading the view.
     }
     
     func bruteForce(passwordToUnlock: String) {
-        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
-
-        var password: String = ""
-
-        // Will strangely ends at 0000 instead of ~~~
-        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-//             Your stuff here
-            print(password)
-            // Your stuff here
-        }
+        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
         
+        var password: String = ""
+        
+        while password != passwordToUnlock {
+        
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+            
+            print(password)
+            
+        }
+    
         print(password)
     }
 }
@@ -50,9 +69,7 @@ extension String {
     var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
     var letters:     String { return lowercase + uppercase }
     var printable:   String { return digits + letters + punctuation }
-
-
-
+    
     mutating func replace(at index: Int, with character: Character) {
         var stringArray = Array(self)
         stringArray[index] = character
@@ -66,24 +83,24 @@ func indexOf(character: Character, _ array: [String]) -> Int {
 
 func characterAt(index: Int, _ array: [String]) -> Character {
     return index < array.count ? Character(array[index])
-                               : Character("")
+    : Character("")
 }
 
 func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
     var str: String = string
-
+    
     if str.count <= 0 {
         str.append(characterAt(index: 0, array))
     }
     else {
         str.replace(at: str.count - 1,
                     with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
+        
         if indexOf(character: str.last!, array) == 0 {
             str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
         }
     }
-
+    
     return str
 }
 
