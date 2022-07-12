ff8a8b900f52661a66eade138249b2f67d1753ee
Andrik <masmay.pl@gmail.com>
2022-07-12 22:28:54 +0200

    add queue

--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -5,28 +5,30 @@ class ViewController: UIViewController {
     let lenPassword = 3
     
     @IBOutlet weak var searchButton: UIButton!
-    
     @IBOutlet weak var label: UILabel!
-    
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var activiti: UIActivityIndicatorView!
-    
     @IBOutlet weak var generateButton: UIButton!
-    
     @IBOutlet weak var cancelButton: UIButton!
     
     @IBAction func cancelBut(_ sender: Any) {
         label.text = ""
         textField.text = ""
+        activiti.stopAnimating()
     }
     
+    @IBAction func eye(_ sender: Any) {
+        textField.isSecureTextEntry.toggle()
+    }
+
     @IBAction func searchBut(_ sender: Any) {
-        self.bruteForce(passwordToUnlock: textField.text ?? "")
-        label.text = textField.text
-        activiti.hidesWhenStopped = true
-        activiti.stopAnimating()
-        textField.isSecureTextEntry = false
+        activiti.startAnimating()
+        label.text = "Wait"
+        bruteForce(passwordToUnlock: textField.text ?? "", completion: { [weak self] password in
+            self?.label.text = password
+            self?.activiti.stopAnimating()
+            self?.textField.isSecureTextEntry = false
+        })
     }
     
     @IBAction func onBut(_ sender: Any) {
@@ -34,73 +36,33 @@ class ViewController: UIViewController {
         let randomPassword = String((0..<lenPassword).map{ _ in passwordCharacters[Int(arc4random_uniform(UInt32(passwordCharacters.count)))]})
         textField.text = randomPassword
         textField.isSecureTextEntry = true
-//        activiti.startAnimating()
         label.text = "Password generated"
-        print(randomPassword)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         activiti.hidesWhenStopped = true
-        
     }
     
-    func bruteForce(passwordToUnlock: String) {
-        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
+    func bruteForce(passwordToUnlock: String, completion: @escaping (String) -> Void) {
         
-        var password: String = ""
-        
-        while password != passwordToUnlock {
-        
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+        let queue = DispatchQueue(label: "OneQueue")
+     
+        queue.async {
+           
+            let allowedCharacters: [String] = String().printable.map { String($0) }
+            var password: String = ""
             
-            print(password)
+            while password != passwordToUnlock {
+                
+                password = generateBruteForce(password, fromArray: allowedCharacters)
+            }
             
-        }
-    
-        print(password)
-    }
-}
-
-extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation }
-    
-    mutating func replace(at index: Int, with character: Character) {
-        var stringArray = Array(self)
-        stringArray[index] = character
-        self = String(stringArray)
-    }
-}
-
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index])
-    : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-    
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-        
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            DispatchQueue.main.async {
+                completion(password)
+               
+            }
         }
     }
-    
-    return str
 }
 
