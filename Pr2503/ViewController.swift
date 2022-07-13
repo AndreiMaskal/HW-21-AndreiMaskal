1c87bdcbb7b7dd0595a47dc84d3816065f8035c3
Andrik <masmay.pl@gmail.com>
2022-07-13 19:25:24 +0200

    final commit

--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -2,21 +2,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let lenPassword = 3
-    
+    let lenghtPassword = 3
+ 
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var activiti: UIActivityIndicatorView!
+    @IBOutlet weak var spiner: UIActivityIndicatorView!
     @IBOutlet weak var generateButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var passwordLabel: UILabel!
     
-    
     @IBAction func cancelBut(_ sender: Any) {
         label.text = ""
         textField.text = ""
-        activiti.stopAnimating()
+        spiner.stopAnimating()
         passwordLabel.text = ""
     }
     
@@ -25,30 +24,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func searchBut(_ sender: Any) {
-        activiti.startAnimating()
+        spiner.startAnimating()
         label.text = "Wait"
         bruteForce(passwordToUnlock: textField.text ?? "", completion: { [weak self] password in
             self?.label.text = "Password found"
-            self?.activiti.stopAnimating()
+            self?.spiner.stopAnimating()
             self?.textField.isSecureTextEntry = false
+     
         })
     }
     
     @IBAction func onBut(_ sender: Any) {
-        let passwordCharacters = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-        let randomPassword = String((0..<lenPassword).map{ _ in passwordCharacters[Int(arc4random_uniform(UInt32(passwordCharacters.count)))]})
-        textField.text = randomPassword
+        generatePassword()
         textField.isSecureTextEntry = true
         label.text = "Password generated"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activiti.hidesWhenStopped = true
+        spiner.hidesWhenStopped = true
+        textField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
+        textField.delegate = self
     }
     
     func shouPassword(password: String) {
         passwordLabel.text = password
+    }
+    
+    func generatePassword() {
+        let passwordCharacters = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-+=")
+        let randomPassword = String((0..<lenghtPassword).map{ _ in passwordCharacters[Int(arc4random_uniform(UInt32(passwordCharacters.count)))]})
+        textField.text = randomPassword
     }
     
     func bruteForce(passwordToUnlock: String, completion: @escaping (String) -> Void) {
@@ -77,3 +83,15 @@ class ViewController: UIViewController {
     }
 }
 
+    extension ViewController: UITextFieldDelegate {
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textFieldText = textField.text,
+              let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+        return false
+    }
+        let subsStringToReplace = textFieldText[rangeOfTextToReplace]
+        let count = textFieldText.count - subsStringToReplace.count + string.count
+        return count <= 3
+    }
+}
